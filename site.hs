87f7476ad57ee169afa26c5046ee3bc1e5fe09cd
Alexander Krupenkin <mail@akru.me>
2017-09-06 09:23:05 +0300

    Simple Hakyll blog init

---
+++ site.hs
@@ -0,0 +1,47 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import qualified GHC.IO.Encoding as E
+import           Data.Monoid (mappend)
+import           Hakyll
+
+--------------------------------------------------------------------------------
+blog :: IO ()
+blog = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateBodyCompiler
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%Y-%m-%d" `mappend`
+    defaultContext
+
+main :: IO ()
+main = E.setLocaleEncoding E.utf8 >> blog
